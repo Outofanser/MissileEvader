9325f24ad33f9d7de6c97a5967408661e97176b5
Evan Moen <evan.moen.2@gmail.com>
2022-11-28 19:26:21 -0500

    Updated player controller, adjusted missile prefab, and added MissileController to guide to player

---
+++ Assets/Scripts/MissileController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileController : MonoBehaviour
+{
+    public GameObject target;
+    public float airSpeed = 10;
+    public float gain = 3;
+
+    public Vector3 relativePosition;
+    public Vector3 relativeVelocity;
+    public Vector3 accelerationCmd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get target position and velocity in inertial world frame
+        Vector3 targetPosition = target.transform.position;
+        Vector3 targetVelocity = target.transform.forward * target.GetComponent<PlayerController>().airSpeed;
+        // get missile position and velocity in intertial world frame
+        Vector3 missilePosition = transform.position;
+        Vector3 missileVelocity = transform.forward * airSpeed;
+        // get instantaneous relative position and velocity in world frame (note, relative rotation of local frame and world frame means the frame is relevant!)
+        relativePosition = targetPosition - missilePosition;
+        relativeVelocity = targetVelocity - missileVelocity;
+
+
+        // get acceleration command and body rotation rate (in world frame)
+        accelerationCmd = PureProNav(relativePosition, relativeVelocity, gain);
+        Vector3 bodyRate = Vector3.Cross(transform.forward, accelerationCmd / airSpeed);
+
+        // convert to local frame
+        Vector3 bodyRateLocal = transform.InverseTransformVector(bodyRate);
+
+        // Apply missile movement
+        transform.Translate(Vector3.forward * airSpeed * Time.deltaTime);
+        transform.Rotate(bodyRateLocal * Mathf.Rad2Deg * Time.deltaTime);//
+    }
+
+    Vector3 PureProNav(Vector3 relPos, Vector3 relVel, float gain)
+    {
+        Vector3 Vm = transform.forward * airSpeed;
+        Vector3 LoSRate = Vector3.Cross(relPos, relVel) / (relPos.magnitude * relPos.magnitude);
+
+        float speedRatio = relVel.magnitude / Vm.magnitude;
+
+        Vector3 accelerationCmd = -gain * speedRatio * Vector3.Cross(Vm, LoSRate);
+
+        return accelerationCmd;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,38 +7,34 @@ public class PlayerController : MonoBehaviour
     public float pitchRate = 20; // deg/s
     public float rollRate = 30;
     public float yawRate = 10;
-    public float airSpeed = 30;
-    public GameObject centerOfMassObject;
+    public float airSpeed = 30; // m/s
 
     // Make these private
-    public Quaternion currentRotation;
-    public Quaternion attitudeRate;
     public Vector3 acceleration;
-    private Vector3 centerOfMass;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        currentRotation = transform.rotation;
-        centerOfMass = centerOfMassObject.transform.localPosition;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Move player forward in forward direction (not realistic physics)
+        transform.Translate(Vector3.forward * airSpeed * Time.deltaTime);
+
+        // get inputs to attitude controls
         Vector3 playerAttitudeInput = new Vector3(-Input.GetAxis("Pitch"), -Input.GetAxis("Yaw"), -Input.GetAxis("Roll"));
-
-        attitudeRate.eulerAngles = playerAttitudeInput;
-        acceleration = Vector3.Cross(Vector3.forward,attitudeRate.eulerAngles) * airSpeed; // may be useful information to use truth data with missile system
-
-        currentRotation.eulerAngles = Vector3.Scale(playerAttitudeInput, new Vector3(pitchRate,yawRate,rollRate)*Time.deltaTime);
-
-        transform.rotation = currentRotation;
-        transform.position += Vector3.forward * airSpeed;
-
-
-
+        // scale inputs to body moments (body rates)
+        Vector3 rotationRate = Vector3.Scale(playerAttitudeInput, new Vector3(pitchRate,yawRate,rollRate));
         
+        acceleration = Vector3.Cross(Vector3.forward * airSpeed, rotationRate); // may be useful information to use truth data with missile system
+
+        // Rotate player via inputs
+        transform.Rotate(rotationRate*Time.deltaTime);
+  
 
     }
 }
