766bc62bcd9eda8fe209c58dd55a0a5e49168feb
Evan Moen <evan.moen.2@gmail.com>
2024-05-13 19:04:12 -0400

    Made the missile movement via forces and attack angle manipulation. Needs cleanup

--- Assets/Scripts/MissileController.cs
+++ Assets/Scripts/MissileController.cs
@@ -1,30 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 
 public class MissileController : MonoBehaviour
 {
     public GameObject target;
-    public float airSpeed = 10;
+    public Rigidbody body;
+    public float airSpeed = 50;
     public float gain = 3;
-    public float maneuverability = 1;
+    public float aoa_limit_deg = 10;
+    public float maneuverability = 10;
+    public float control_limit_degps = 1;
+    
 
     public Vector3 relativePosition;
     public Vector3 relativeVelocity;
     public Vector3 accelerationCmd;
     public float turnSpeed;
     public float tgo;
+    public float attack_limited;
+    public float attackAngle;
+    public Vector3 lift_direction;
+    public Vector3 local_lift;
+    public Vector3 control;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
+        body.AddForce(Vector3.up * airSpeed, ForceMode.VelocityChange);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //MissileMovement();
+    }
+
+    void FixedUpdate()
+    {
         MissileMovement();
+        
+
     }
 
     void MissileMovement()
@@ -34,7 +52,8 @@ public class MissileController : MonoBehaviour
         Vector3 targetVelocity = target.transform.forward * target.GetComponent<PlayerController>().airSpeed;
         // get missile position and velocity in intertial world frame
         Vector3 missilePosition = transform.position;
-        Vector3 missileVelocity = transform.forward * airSpeed;
+        ///Vector3 missileVelocity = transform.forward * airSpeed;
+        Vector3 missileVelocity = body.velocity;
         // get instantaneous relative position and velocity in world frame (note, relative rotation of local frame and world frame means the frame is relevant!)
         relativePosition = targetPosition - missilePosition;
         relativeVelocity = targetVelocity - missileVelocity;
@@ -43,22 +62,67 @@ public class MissileController : MonoBehaviour
 
 
         // get acceleration command and body rotation rate (in world frame)
-        accelerationCmd = PureProNav(relativePosition, relativeVelocity, gain);// + Vector3.up * 9.8f * gain/2;
-        Vector3 bodyRate = Vector3.Cross(transform.forward, accelerationCmd / airSpeed);
+  
+        accelerationCmd = PureProNav(missileVelocity,relativePosition, relativeVelocity, gain);// + Vector3.up * 9.8f * gain/2;
 
+        Vector3 bodyRate = Vector3.Cross(transform.forward, accelerationCmd / airSpeed);
         bodyRate = RestrictBodyRate(bodyRate);
 
         // convert to local frame
         Vector3 bodyRateLocal = transform.InverseTransformVector(bodyRate);
 
         // Apply missile movement
-        transform.Translate(Vector3.forward * airSpeed * Time.deltaTime);
-        transform.Rotate(bodyRateLocal * Mathf.Rad2Deg * Time.deltaTime);//
+        //transform.Translate(Vector3.forward * airSpeed * Time.deltaTime);
+        //transform.Rotate(bodyRateLocal * Mathf.Rad2Deg * Time.deltaTime);//
+        //
+
+        Vector3 forward = body.transform.forward;
+
+        Vector3 temp1 = Vector3.Cross(forward, missileVelocity);
+        Vector3 temp2 = Vector3.Cross(missileVelocity, temp1);
+        lift_direction = temp2.normalized;
+
+        float vel = missileVelocity.magnitude;
+        float area = 0.01f;
+        attackAngle = Vector3.Angle(forward, missileVelocity);
+
+        float dAoA = accelerationCmd.magnitude / area / vel / vel / (2 * Mathf.PI);
+        attack_limited = Mathf.Min(dAoA*Mathf.Rad2Deg, aoa_limit_deg);
+        Vector3 turnDir = Vector3.Cross(missileVelocity, accelerationCmd).normalized;
+
+
+        float lift_coefficient = 2 * Mathf.PI * attackAngle * Mathf.Deg2Rad;
+        float drag_coefficient = 2 * Mathf.PI * Mathf.Pow(attackAngle*Mathf.Deg2Rad,4f);
+        if (attackAngle > 12)
+        {
+            lift_coefficient = Mathf.Sin(2f*attackAngle*Mathf.Deg2Rad);
+            drag_coefficient = 1f - Mathf.Cos(2f*attackAngle*Mathf.Deg2Rad);
+        }
+
+
+        Vector3 lift_force = lift_direction * lift_coefficient * vel * vel * area;
+        Vector3 drag_force = -missileVelocity.normalized * drag_coefficient * vel * vel * area;
+ 
+        Vector3 desiredForward = Quaternion.AngleAxis(attack_limited, turnDir) * missileVelocity.normalized;
+        Vector3 angle = -Vector3.Cross(desiredForward, forward);
+        float phi = Mathf.Asin(angle.magnitude)/Time.fixedDeltaTime;
+        control = (phi * angle.normalized)/Time.fixedDeltaTime - body.angularVelocity;
+        control = control * Mathf.Min(control_limit_degps, control.magnitude) / control.magnitude;
+        
+
+        //body.AddRelativeForce(local_lift,ForceMode.Acceleration);// + drag_force);
+        body.AddForce(lift_force+drag_force,ForceMode.Acceleration);
+
+        //body.AddForce(lift_force, ForceMode.Acceleration);
+
+        if (control.magnitude > 0.001)
+        {
+            body.AddTorque(control, ForceMode.Acceleration);
+        }
     }
 
-    Vector3 PureProNav(Vector3 relPos, Vector3 relVel, float gain)
+    Vector3 PureProNav(Vector3 Vm, Vector3 relPos, Vector3 relVel, float gain)
     {
-        Vector3 Vm = transform.forward * airSpeed;
         Vector3 LoSRate = Vector3.Cross(relPos, relVel) / (relPos.magnitude * relPos.magnitude);
 
         float speedRatio = relVel.magnitude / Vm.magnitude;
